80ceb255fbf2663e5cc5fc815df73336b8f63ed6
Silfrido Reverol <silfridoer@gmail.com>
2021-03-16 16:55:41 -0400

    login and register routes, auth middleware created

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const auth = require('../../middleware/auth');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-const User = require('../../models/User');
+
+//get user through token
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 router.post(
 	'/',
 	[
-		check('name', 'Name is required').not().isEmpty(),
 		check('email', 'Please include a valid email').isEmail(),
-		check(
-			'password',
-			'Please enter a password with 6 or more characters'
-		).isLength({ min: 6 }),
+		check('password', 'Password is required').exists(),
 	],
 	async (req, res) => {
 		const errors = validationResult(req);
@@ -22,28 +30,24 @@ router.post(
 			return res.status(400).json({ errors: errors.array() });
 		}
 
-		const { name, email, password } = req.body;
+		const { email, password } = req.body;
 
 		try {
 			let user = await User.findOne({ email });
 
-			if (user) {
+			if (!user) {
 				return res
 					.status(400)
-					.json({ errors: [{ msg: 'User already exists' }] });
+					.json({ errors: [{ msg: 'Invalid Credentials' }] });
 			}
 
-			user = new User({
-				name,
-				email,
-				password,
-			});
+			const isMatch = await bcrypt.compare(password, user.password);
 
-			const salt = await bcrypt.genSalt(10);
-
-			user.password = await bcrypt.hash(password, salt);
-
-			await user.save();
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid Credentials' }] });
+			}
 
 			const payload = {
 				user: {
--- server.js
+++ server.js
@@ -5,6 +5,11 @@ connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server initialized on port ${PORT}`));
