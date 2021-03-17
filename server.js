db1a1b4b37da685fcd1ee220c077d9b750b75398
Silfrido Reverol <silfridoer@gmail.com>
2021-03-16 22:34:12 -0400

    cors just to try frontend

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json({ extended: false }));
 
